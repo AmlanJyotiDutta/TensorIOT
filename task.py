b2fbcaba377b3b413e1da15eda6d9906f4d476c1
AmlanJyotiDutta <74617182+AmlanJyotiDutta@users.noreply.github.com>
2023-11-23 17:59:45 +0530

    Update task.py
    Changes to make the code more readable

--- task.py
+++ task.py
@@ -7,19 +7,29 @@ from dotenv import load_dotenv
 import json
         
 class ParkingSlot: 
+    # defining the constrcutor for ParkingSlot Class 
+    # it takes two values: size of the parking space and the dimension of a parking slot
+    # The contructor create the array with slots that can fit in the parking space
     def __init__(self, size = 2000 , parking_spot_size = (8, 12)):
         slot_num = size // (parking_spot_size[0] * parking_spot_size[1])
         self.slots = [None for _ in range(slot_num)]
     
+
+    # Method to check whether a parking slot is empty
     def _is_slot_empty(self, slot_num) -> bool :
         if slot_num >= len(self.slots): 
             raise ValueError(f'Slot number must be >=1 && < {len(self.slots)}')
         return self.slots[slot_num] is None 
+    
+    #Method to find empty parking slot in the parking space
     def find_empty_slot(self):
         for idx,slot in enumerate(self.slots):
             if slot is None:
                 return idx
         return -1    
+    
+    # Method to insert into the paking slot array a car license
+    # takes a slot number adn car license as input and if the slot number is not occupied , replaces the None value of the array with the car license
     def insert_slot(self, slot_num, car ): 
         try: 
             is_empty = self._is_slot_empty(slot_num)
@@ -32,7 +42,8 @@ class ParkingSlot:
         except ValueError as ex:
             #logging.error("Failed to insert slot ", ex)
             return -1 
-    
+    # Method to write a json containing the license number of a car and its parking slot
+    # The Method also uploads the written file to an s bucket
     def export_state(self,filename,write_to_s3 = False):
         parked_car_dict ={}
         for idx,car in enumerate(self.slots):
@@ -47,7 +58,9 @@ class ParkingSlot:
             s3.upload_file(filename, 'amlan-tensor-iot-test-bucket', filename)
         
             
-class Car: 
+class Car:
+    # Constrcutor of the Car class 
+    # Takes input the license, if License is not procided it generates a random 7 digit license number 
     def __init__(self, license = None):
         if license is None: 
             self.license = self._generate_license()
@@ -58,16 +71,17 @@ class Car:
     def _generate_license(self): 
         res = ''.join(random.choices(string.ascii_uppercase +string.digits, k = 7))
         return res
-
+    # Dunder method to return a string representation of the class object
     def __str__(self): 
-        return f'[License [{self.license}]]'
-
+        return f'{self.license}'
+    # Method which inputs slot and the parking slot array and internally calls te insert_slot method to park a car in a slot
     def park(self, slot: int, parking_slot):
         if parking_slot is None: 
             raise ValueError("Car cannot be parked without a parking slot ")
         park_status = parking_slot.insert_slot(slot, self)
         return park_status
-        
+# main method interates through the array of cars and an array of random slots assigned to them and inserts the cars in the parking slot array 
+# till the car array is completed or the slots in the parking_slot array runs out        
 def main():
     parking_slot = ParkingSlot()
     cars = [Car() for _ in range(21)]
@@ -77,17 +91,18 @@ def main():
         park_status = car.park(random_slot, parking_slot)
         
         if  park_status !=-1 :
-            print(f"Car {car} inserted successfully at slot {random_slot}")
+            print(f"Car with license plate {car} parked successfully in spot {random_slot}")
         else: 
-            print(f'random_slot {random_slot} car{car} failed . trying to insert in successive slots ')
+            print(f'Car with license plate {car} unable to be parked in spot {random_slot} as it is occupied. trying to insert in successive slots ')
             empty_slot = parking_slot.find_empty_slot() 
             if empty_slot != -1:
                 assert(car.park(empty_slot,parking_slot)!=-1)
-                print(f'Car {car} inserted successfully at {empty_slot}')
+                print(f"Car with license plate {car} parked successfully in spot {empty_slot}")
             else:
-                print(f' Car {car} insertion failed , No empty slot available')
+                print(f' Car with license plate {car} could not be parked , No empty slot available')
     parking_slot.export_state('state.json', write_to_s3 = True)
     
 if __name__ == '__main__': 
+    # library to load the env variable to connect to S3 bucket
     load_dotenv()
     main()
